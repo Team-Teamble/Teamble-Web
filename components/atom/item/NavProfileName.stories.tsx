9060e137ce2f6f7e207095509f51288dc45fc04c
younyikim <rlakuku1221@gmail.com>
2022-01-09 10:46:24 +0900

    feat : Atom - nav 프로필 이름

---
+++ components/atom/item/NavProfileName.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { NavProfileName, NavProfileNameProps } from "./NavProfileName";
+
+export default {
+  title: "atom/item",
+  component: NavProfileName,
+} as Meta;
+
+const Template: Story<NavProfileNameProps> = (args) => {
+  return <NavProfileName {...args} />;
+};
+
+export const Profilename = Template.bind({});
---
+++ components/atom/item/NavProfileName.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface NavProfileNameProps {
+  className?: string;
+  value: string;
+
+  onClick(): void;
+}
+
+export function NavProfileName(props: NavProfileNameProps) {
+  const { className, value = "마가렛", onClick } = props;
+  return (
+    <StyledProfileName className={className} onClick={onClick}>
+      {value}
+    </StyledProfileName>
+  );
+}
+
+const StyledProfileName = styled.a`
+  cursor: pointer;
+
+  font-size: 1em;
+  font-weight: 500;
+`;

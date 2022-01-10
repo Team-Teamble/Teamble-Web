ae35d67e32bfcc3cb867e8426f83092959010f8b
n0eyes <doll4586633@naver.com>
2022-01-10 17:29:10 +0900

    feat: 프로젝트 만들기 페이지 공통 버튼

---
+++ components/atom/button/MakeProjectButton.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { MakeProjectButtonProps, MakeProjectButton } from "./MakeProjectButton";
+
+export default {
+  title: "atom/button/MakeProjectButton",
+  component: MakeProjectButton,
+} as Meta;
+
+const Template: Story<MakeProjectButtonProps> = (args) => {
+  return <MakeProjectButton {...args} />;
+};
+
+export const Basic = Template.bind({});
+Basic.args = {
+  children: "성향",
+} as MakeProjectButtonProps;

d6186b44d782aae90759ac0f426ddb3231ca5887
younyikim <rlakuku1221@gmail.com>
2022-01-13 13:12:00 +0900

    refactor : AlertImage 수정

--- components/atom/image/AlertImage.tsx
+++ components/atom/image/AlertImage.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import AlertIcon from "../../../assets/svg/ic_bell_off.svg";
 
 export interface AlertImageProps {
   className?: string;
-  src?: string;
 
   onClick(): void;
 }
 
 export function AlertImage(props: AlertImageProps) {
-  const { className, src, onClick } = props;
+  const { className, onClick } = props;
   return (
     <StyledAlertWrapper className={className} onClick={onClick}>
-      <StyledAlertImg className={className} src={src} />
+      <AlertIcon />
     </StyledAlertWrapper>
   );
 }
@@ -21,7 +21,7 @@ const StyledAlertWrapper = styled.a`
   cursor: pointer;
 `;
 
-const StyledAlertImg = styled.img`
+const StyledAlertImg = styled.svg`
   width: 2.39em;
   height: 2.69em;
 `;

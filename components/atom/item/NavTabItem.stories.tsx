9d4cb1849fd0430d86be9c38d91feba975ebcba6
younyikim <rlakuku1221@gmail.com>
2022-01-14 01:59:52 +0900

    fix : 에러 해결

--- components/atom/item/NavTabItem.stories.tsx
+++ components/atom/item/NavTabItem.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, Story } from "@storybook/react/types-6-0";
-import { NavTabItem, NavTabItemProps } from "./NavTabItem";
+import NavTabItem, { NavTabItemProps } from "./NavTabItem";
 
 export default {
   title: "atom/item",

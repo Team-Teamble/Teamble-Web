ced66a6b1b69c0d6761a78eb69db035defc8f5cf
Tekiter <tekiter8@gmail.com>
2022-01-15 15:55:34 +0900

    feat: 로그인 만료시간 변경

--- pages/api/auth/store.ts
+++ pages/api/auth/store.ts
@@ -1,12 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Cookies from "cookies";
+import { addDays } from "date-fns";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const cookies = new Cookies(req, res);
 
   if (req.body.accessToken) {
     const accessToken = req.body.accessToken + "";
-    cookies.set("accessToken", accessToken);
+    cookies.set("accessToken", accessToken, {
+      expires: addDays(new Date(), 7),
+    });
   }
 
   res.status(200).json({ message: "success" });
